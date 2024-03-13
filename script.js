7c98e930de186c516b6152acdb4719e9aaa4bc47
vishwamohini <vishwamohini0015@gmail.com>
2024-03-13 08:18:56 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,90 @@
+const temperaturefield = document.querySelector(".weather1")
+const cityfield = document.querySelector(".weather2 p")
+const datefield = document.querySelector(".weather2 span")
+const emojifield = document.querySelector(".weather3 img")
+const weatherfield = document.querySelector(".weather3 span")
+const searchfield=document.querySelector(".searchfield")
+const form=document.querySelector("form")
+//adding event listner to the form
+form.addEventListener("submit",search)
+
+//default location
+let target = "delhi"
+
+//function to fetch datafrom weather api
+const fetchData = async (target) => {
+    const url = `http://api.weatherapi.com/v1/current.json?key=3a204a68f6694589a05181859241003&q=${target}`
+
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+
+//destructuring
+    const {
+        current: {
+             temp_c,
+            condition: { text, icon },
+        },
+
+
+        location: { name, localtime },
+    } = data;
+
+//calling update function
+    updateDom(temp_c, name, localtime, icon, text);
+} 
+//function to update dom
+function updateDom(temperature, city, time, emoji, text) {
+    const exactTime = time.split(" ")[1];
+    const exactDate = time.split(" ")[0];
+    const exactDay = new Date(exactDate).getDay()
+
+    temperaturefield.innerText = temperature
+    cityfield.innerText = city;
+
+    console.log(getDayfullname(0));
+    datefield.innerText=`${exactTime}- ${getDayfullname(exactDay)} - ${exactDate}`
+
+    emojifield.src = emoji
+    weatherfield.innerText = text
+}
+
+fetchData(target)
+
+
+
+//function to search the location
+function search(e){
+    e.preventDefault();
+
+    target=searchfield.value;
+    fetchData(target)
+    console.log(target);
+}
+
+//function to get the  name of day
+function getDayfullname(num) {
+    switch (num) {
+        case 0:
+            return "sunday"
+
+        case 1:
+            return "Monday"
+
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thrusday"
+        case 5:
+            return "friday"
+        case 6:
+            return "saturday"
+
+        default:
+            return "nothing";
+    }
+}
